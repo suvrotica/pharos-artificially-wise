c660733ae9b8b15b343610a7b8ba108b34119983
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-10-28 09:44:45 +0530

    v1

--- src/lib/components/youtube.svelte
+++ src/lib/components/youtube.svelte
@@ -17,7 +17,7 @@
     controls ? '' : 'controls=0&amp;'
   }${fs ? '' : 'fs=0&amp;'}${
     loop ? 'loop=1&amp;' : ''
-  }iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1&amp;origin=${encodeURI(
+  }mute=1&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1&amp;origin=${encodeURI(
     siteConfig.url,
   )}`;
 </script>
